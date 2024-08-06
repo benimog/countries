8d2dfec4f300c5a8c37e0d0115b4e1b945b80f47
Martin Andersson <martin.artan.andersson@gmail.com>
2024-08-06 21:33:16 +0200

    Info about usage of number keys

--- countries/src/components/About.tsx
+++ countries/src/components/About.tsx
@@ -5,21 +5,18 @@ function About() {
   return (
     <div>
       <h1>Välkommen till flaggquiz.se</h1>
+
       <p>
-        Här går det att se en{" "}
-        <Link href="/countries" color="inherit">
-          lista över länder och regioner
-        </Link>{" "}
-        och spela ett{" "}
-        <Link href="/flags" color="inherit">
-          flaggquiz
-        </Link>
-        .<br /> Det går även att spela ett lite svårare{" "}
-        <Link href="/write" color="inherit">
-          flaggquiz
-        </Link>
-        .
+        <b>Tips!</b>
+        <br />
+        Det går att använda tangentbordet för att svara.
       </p>
+      <img
+        src="/numpad.png"
+        alt="Använd tangenterna 7, 8, 4, 5"
+        style={{ maxWidth: "80vw" }}
+      />
+      <p>Använd tangenterna 7, 8, 4, 5 enligt bilden</p>
       <div className="footer">
         <p>
           <a
